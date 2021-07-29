b95204563abc12a7124e93655065b8f26a1bf33a
Dmitry Efremov <kykareky05@mail.ru>
2021-07-29 19:03:21 +0300

    1

---
+++ fixture/group.py
@@ -0,0 +1,63 @@
+
+
+class GroupHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init goup creation
+        wd.find_element_by_name("new").click()
+        # fill group form
+        self.fill_group_form(group)
+        # submit
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def open_groups_page(self):
+        wd = self.app.wd
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_element_by_name("selected[]"))
---
+++ test/test_add_group.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from model.group import Group
+
+
+    
+def test_add_group(app):
+    app.group.create(Group(name="ewq", header="ewq1", footer="ewq12"))
+
+
+
+def test_add_empty_group(app):
+    app.group.create(Group(name="", header="", footer=""))
+
+
